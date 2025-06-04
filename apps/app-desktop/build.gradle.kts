a5697261a656e054d198f848437f2231625d5c66
Jendrik Johannes <jendrik.johannes@gmail.com>
2025-06-04 21:07:44 +0200

    Fix app module setup

--- apps/app-desktop/build.gradle.kts
+++ apps/app-desktop/build.gradle.kts
@@ -9,6 +9,10 @@ distributions.main {
   contents.from(configurations.runtimeClasspath)
 }
 
+tasks.assemble {
+  dependsOn(tasks.installDist)
+}
+
 dependencies {
   runtimeOnly(project(":base-engine"))
   runtimeOnly(project(":renderer-lwjgl"))
--- docs/presentation/src/main/assets/javarcade-no-modules/apps/app-desktop/build.gradle.kts
+++ docs/presentation/src/main/assets/javarcade-no-modules/apps/app-desktop/build.gradle.kts
@@ -1,19 +1,23 @@
 plugins {
-    id("dependency-rules")
-    id("repositories")
-    id("distribution")
-    id("java")
+  id("dependency-rules")
+  id("repositories")
+  id("distribution")
+  id("java")
 }
 
 distributions.main {
-    contents.from(configurations.runtimeClasspath)
+  contents.from(configurations.runtimeClasspath)
+}
+
+tasks.assemble {
+  dependsOn(tasks.installDist)
 }
 
 dependencies {
-    runtimeOnly(project(":base-engine"))
-    runtimeOnly(project(":renderer-lwjgl"))
-    runtimeOnly(project(":classic-assets"))
-    runtimeOnly(project(":classic-items"))
-    runtimeOnly(project(":classic-levels"))
-    runtimeOnly("org.slf4j:slf4j-simple")
+  runtimeOnly(project(":base-engine"))
+  runtimeOnly(project(":renderer-lwjgl"))
+  runtimeOnly(project(":classic-assets"))
+  runtimeOnly(project(":classic-items"))
+  runtimeOnly(project(":classic-levels"))
+  runtimeOnly("org.slf4j:slf4j-simple")
 }
