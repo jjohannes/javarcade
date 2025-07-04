0779a7945de28e49f6c24d8e36bfc9619f4c7c43
Jendrik Johannes <jendrik.johannes@gmail.com>
2025-07-04 13:14:47 +0200

    Remove module-info.java files and define dependencies in build

--- game/jamcatch-stage/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module de.javarca.jamcatch.stage {
-  requires transitive de.javarca.model;
-  requires org.slf4j;
-
-  exports de.javarca.jamcatch.stage;
-
-  provides de.javarca.model.Stage with
-          de.javarca.jamcatch.stage.JamCatchStage;
-}
