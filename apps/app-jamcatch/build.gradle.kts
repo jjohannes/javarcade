069467aaab844efe7a666652e36efe230d9b08cc
Jendrik Johannes <jendrik.johannes@gmail.com>
2025-09-04 09:26:20 +0200

    Example for building a fat Jar with all natives

--- apps/app-jamcatch/build.gradle.kts
+++ apps/app-jamcatch/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
   id("de.javarca.build.dependency-rules")
   id("de.javarca.build.repositories")
   id("application")
+  id("com.gradleup.shadow") version "9.1.0"
 }
 
 application {
@@ -24,4 +25,12 @@ dependencies {
   runtimeOnly(project(":jamcatch-actors"))
   runtimeOnly(project(":jamcatch-stage"))
   runtimeOnly("org.slf4j:slf4j-simple")
+}
+
+tasks.shadowJar {
+  configurations = listOf(
+    project.configurations.macRuntimeClasspath.get(),
+    project.configurations.macArmRuntimeClasspath.get(),
+    project.configurations.winRuntimeClasspath.get()
+  )
 }
