f0dce5769bbc2189e1632f8993ec651ace1b1592
Jendrik Johannes <jendrik.johannes@gmail.com>
2025-09-12 11:26:32 +0200

    Change plugin order for readability

--- gradle/plugins/src/main/kotlin/de.javarca.build.java-module.gradle.kts
+++ gradle/plugins/src/main/kotlin/de.javarca.build.java-module.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
+  id("java-library")
   id("de.javarca.build.dependency-rules")
   id("de.javarca.build.repositories")
   id("de.javarca.build.compile")
   id("de.javarca.build.test")
   id("de.javarca.build.quality-check")
-  id("java-library")
 }
