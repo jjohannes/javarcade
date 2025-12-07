e11c8e9613a4650c7688f898b8c635443928f7d3
Jendrik Johannes <jendrik.johannes@gmail.com>
2025-12-07 19:58:08 +0100

    Update org.gradlex.jvm-dependency-conflict-resolution to 2.5

--- gradle/plugins/build.gradle.kts
+++ gradle/plugins/build.gradle.kts
@@ -4,7 +4,7 @@ repositories { gradlePluginPortal() }
 
 dependencies {
     implementation("com.autonomousapps:dependency-analysis-gradle-plugin:3.5.1")
-    implementation("org.gradlex:jvm-dependency-conflict-resolution:2.4")
+    implementation("org.gradlex:jvm-dependency-conflict-resolution:2.5")
     implementation("org.gradlex:java-module-dependencies:1.11")
     implementation("org.gradlex:java-module-packaging:1.2")
     implementation("org.gradlex:java-module-testing:1.8")
--- gradle/plugins/src/main/kotlin/de.javarca.build.dependency-rules.gradle.kts
+++ gradle/plugins/src/main/kotlin/de.javarca.build.dependency-rules.gradle.kts
@@ -10,26 +10,6 @@ jvmDependencyConflicts {
   conflictResolution {
     select("org.gradlex:slf4j-impl", "org.slf4j:slf4j-simple")
   }
-
-  patch {
-    listOf("", "-glfw", "-opengl", "-stb").forEach { module ->
-      module("org.lwjgl:lwjgl$module") {
-        addTargetPlatformVariant("natives",
-          "natives-linux", LINUX, X86_64)
-        addTargetPlatformVariant("natives",
-          "natives-linux-arm64", LINUX, ARM64)
-        addTargetPlatformVariant("natives",
-          "natives-macos", MACOS, X86_64)
-        addTargetPlatformVariant("natives",
-          "natives-macos-arm64", MACOS, ARM64)
-        addTargetPlatformVariant("natives",
-          "natives-windows", WINDOWS, X86_64)
-        addTargetPlatformVariant("natives",
-          "natives-windows-arm64", WINDOWS, ARM64)
-      }
-    }
-  }
-
   consistentResolution {
     platform(":versions")
   }
